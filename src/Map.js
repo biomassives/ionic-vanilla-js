02c35d68ab967afe0eaf93dd0afb5e724e303a11
G. Willson <greg@biomassiv.es>
2022-06-15 19:24:57 -0600

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -560,7 +560,7 @@
 		var logo = L.control({position: 'bottomright'});
 		logo.onAdd = function(map){
 			var div = L.DomUtil.create('div', 'myclass');
-			div.innerHTML= "<img src='/src/icons/gcm_logo_april30_2022.svg' height=70 />";
+			div.innerHTML= "<img src='/takatakatembo.svg' height=70 />";
 			return div;
 		}
 		logo.addTo(map);
