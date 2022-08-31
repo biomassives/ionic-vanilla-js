12825746db1ba495edcb1b92bdc64bc6dd46062d
G. Willson <greg@biomassiv.es>
2022-08-30 21:11:41 -0600

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -575,15 +575,18 @@
 		*/
 		document.getElementById("0maleaf").addEventListener("click", function () {
 			clearItems()
+                        document.getElementById("0greeting").style.display = "block";
+
+                });
+		document.getElementById("0maleaf").addEventListener("click", function () {
+			clearItems()
                         document.getElementById("map").style.display = "block";
-                        document.getElementById("greeting").style.display = "block";
 			document.getElementById("mapbuttons").style.display = "block";
 
                 });
 		document.getElementById("switch_to_map").addEventListener("click", function () {
 			clearItems()
                         document.getElementById("map").style.display = "block";
-                        document.getElementById("greeting").style.display = "block";
 			document.getElementById("mapbuttons").style.display = "block";
                 });
 		document.getElementById("switch_to_selecta").addEventListener("click", function () {
